65df4bd167a9f028432cdc3428ad7aab803dbf68
aliciagb14 <aliciagonzalezb14@gmail.com>
2025-01-05 18:08:58 +0100

    login added

--- ce-webPage/src/components/Menu.vue
+++ ce-webPage/src/components/Menu.vue
@@ -1,15 +1,33 @@
 <template>
-    <div>
-      <h1>Gestión de Tiendas</h1>
+    <h1>Gestión de Tiendas</h1>
+    <n-space vertical>
+        <n-input round v-model:value="username" type="text" placeholder="Username" />
+        <n-input round v-model:value="password" type="password" placeholder="Password" />
+    </n-space>
+    <div class="button-container">
+        <n-button type="success" ghost @click="handleLogin" >Login</n-button>
+        <n-button type="error" ghost @click="forgotPassword">Forgot your password?
+            <n-icon><ForgotPasswordIcon/></n-icon>
+        </n-button>
     </div>
 </template>
   
 <script setup>
+import {ref} from 'vue'
+import { NInput, NButton, NCard, NSpace, NIcon } from 'naive-ui'
+import { LockClosedOutline as ForgotPasswordIcon} from '@vicons/ionicons5';
+const username = ref('')
+const password = ref('')
 
+const handleLogin = () => {
+    
+}
 </script>
 
 
 <style scoped>
-
-
+    .button-container {
+        margin-top: 20px;
+        margin-left: 230px;
+    }
 </style>
