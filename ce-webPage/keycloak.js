a98df9d70f658f6694b88b1d20572a92f0805770
LautaroRM <lautaro.rmazzarello@gmail.com>
2025-03-02 19:25:10 +0100

    Cambio en Login, limpieza de estilos y intento de omitir el inicio de sesion de keycloak

--- ce-webPage/keycloak.js
+++ ce-webPage/keycloak.js
@@ -1,40 +1,47 @@
-import Keycloak from 'keycloak-js'
+import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-    url: 'http://localhost:8080',
-    realm: 'ComercioElectronico',
-    clientId: 'frontend-ce'
+  url: "http://localhost:8080",
+  realm: "ComercioElectronico",
+  clientId: "frontend-ce",
 });
 
 let initialized = false;
-keycloak.authServerUrl = 'http://localhost:8080'; 
+keycloak.authServerUrl = "http://localhost:8080";
 
 export const initKeycloak = (isauthenticated) => {
-    if (!initialized) {
-        initialized = true;
-        return keycloak.init({ onLoad: 'login-required' })
-            .then(authenticated => {
-                isauthenticated.value = authenticated; // Actualiza isauthenticated aquí
-                if (authenticated) {
-                    // Extrae el nombre de usuario desde el token decodificado
-                    const username = keycloak.tokenParsed?.preferred_username || 'Usuario desconocido';
-                    console.log(`Authenticated as: ${username}`);
-                    const roles = keycloak.tokenParsed?.realm_access?.roles || [];
-                    console.log(`Role as:` , roles)
-                    const isAdmin = roles.includes('admin');
-                    return { username, isAdmin };
-                }
-            })
-            .catch(error => {
-                console.error('Failed to initialize Keycloak', error);
-            });
-    }
+  if (!initialized) {
+    initialized = true;
+    return keycloak
+      .init({
+        onLoad: "check-sso",
+        silentCheckSsoRedirectUri:
+          window.location.origin + "/silent-check-sso.html",
+      })
+      .then((authenticated) => {
+        isauthenticated.value = authenticated; // Actualiza isauthenticated aquí
+        if (authenticated) {
+          // Extrae el nombre de usuario desde el token decodificado
+          const username =
+            keycloak.tokenParsed?.preferred_username || "Usuario desconocido";
+          console.log(`Authenticated as: ${username}`);
+          const roles = keycloak.tokenParsed?.realm_access?.roles || [];
+          console.log(`Role as:`, roles);
+          const isAdmin = roles.includes("admin");
+          return { username, isAdmin };
+        } else {
+          console.log("Usuario no autenticado, pero sin redirección forzada.");
+        }
+      })
+      .catch((error) => {
+        console.error("Failed to initialize Keycloak", error);
+      });
+  }
 };
 
 export const forgotPassword = () => {
-    const resetPasswordUrl = `${keycloak.authServerUrl}/realms/${keycloak.realm}/account`;
-    window.location.href = resetPasswordUrl;
+  const resetPasswordUrl = `${keycloak.authServerUrl}/realms/${keycloak.realm}/account`;
+  window.location.href = resetPasswordUrl;
 };
-
 
 export default keycloak;
